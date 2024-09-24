ef4c0d6342057b6edaec1c5d320ea48a5bdc0f7e
Gustvo Farias <gustavo.f.farias@hotmail.com>
2024-09-23 21:47:28 -0300

    pe1.1

---
+++ src/data/fs/users.js
@@ -0,0 +1,22 @@
+[
+  {
+    email: "gustavo@gmail.com",
+    password: "147369",
+    id: "b4e30b61f5abb2d5c2d297f8",
+  },
+  {
+    email: "francisco@gmail.com",
+    password: "147369",
+    id: "7242a8a0a9dd0b7e2fd08720",
+  },
+  {
+    email: "lorenzo@gmail.com",
+    password: "147369",
+    id: "4de08cf760a6cb5d7e8ca095",
+  },
+  {
+    email: "emilse@gmail.com",
+    password: "147369",
+    id: "035d07d3b4f8eb3596606518",
+  },
+];
